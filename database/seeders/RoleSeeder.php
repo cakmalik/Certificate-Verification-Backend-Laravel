0f76d71a0caa4cbb6bf65922778c0e9056db8bd2
hasinilmalik <hasinilmalik@gmail.com>
2022-05-22 15:42:25 +0700

    add role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,18 +2,26 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    * Run the database seeds.
+    *
+    * @return void
+    */
     public function run()
     {
-        //
+        DB::table('roles')->insert(
+            [
+                'name' => 'admin',
+            ],
+            [
+                'name' => 'user',
+            ]);
+        }
     }
-}
+    
